f96ae6d843e5de8638e82d44087fe5a06b5addf6
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-05-17 11:23:19 +0200

    kommentar #2

--- app/src/main/java/com/example/sturzapp/MainActivity.java
+++ app/src/main/java/com/example/sturzapp/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //das ist ein Test 1
+        //das ist ein Test 2
     }
 }
