b652ab05a6253e4530b6cb9f0eef38096bb2ada8
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-05-24 12:16:08 +0200

    Button - zurück zur MainActivity

--- app/src/main/java/com/example/sturzapp/risikopatient_erstellen.java
+++ app/src/main/java/com/example/sturzapp/risikopatient_erstellen.java
@@ -2,7 +2,10 @@ package com.example.sturzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class risikopatient_erstellen extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class risikopatient_erstellen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_risikopatient_erstellen);
+
+        Button button = findViewById(R.id.buttonZurückZurMainActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(risikopatient_erstellen.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
