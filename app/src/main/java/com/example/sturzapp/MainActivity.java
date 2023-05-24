36b26ed00210efe9641e2c1dae81d13253f84049
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-05-24 11:05:39 +0200

    variablen in der MainActivity erstellt

--- app/src/main/java/com/example/sturzapp/MainActivity.java
+++ app/src/main/java/com/example/sturzapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.sturzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioButton risikopatient1 = findViewById(R.id.radioButton_Risikopatient1);
+        RadioButton notfallpatient1 = findViewById(R.id.radioButton_Notfallkontakt1);
+
+        EditText editText_emailadresse = findViewById(R.id.editText_Email);
+        EditText editText_passwort = findViewById(R.id.editTextPassword);
+
+        Button button_login = findViewById(R.id.buttonLogin);
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email_adresse = editText_emailadresse.getText().toString();
+                String passwort = editText_passwort.getText().toString();
+
+            }
+        });
+
+        RadioButton risikopatient2 = findViewById(R.id.radioButtonRiskPatient2);
+        RadioButton notfallkontakt2 = findViewById(R.id.radioButton_Notfallkontakt2);
+
+        Button button_accountErstellen = findViewById(R.id.button_createAccount);
+
+        button_accountErstellen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
         //das ist ein Test 3
     }
