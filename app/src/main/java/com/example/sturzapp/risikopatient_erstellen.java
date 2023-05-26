590b4f667c1a72878da0c90ba37fb6f8182bc0d5
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-05-26 11:12:21 +0200

    risikopatient_startseite.java + notfallkontakt_startseite.java

--- app/src/main/java/com/example/sturzapp/risikopatient_erstellen.java
+++ app/src/main/java/com/example/sturzapp/risikopatient_erstellen.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class risikopatient_erstellen extends AppCompatActivity {
 
@@ -14,14 +15,31 @@ public class risikopatient_erstellen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_risikopatient_erstellen);
 
-        Button button = findViewById(R.id.buttonZurückZurMainActivity);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        Button button1 = findViewById(R.id.buttonCreateAccount);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText_e_mail = findViewById(R.id.editTextEmail);
+
+                String e_mail = editText_e_mail.getText().toString();
+
+                Intent intent1 = new Intent(risikopatient_erstellen.this, risikopatient_startseite.class);
+                intent1.putExtra("e_mail", e_mail);
+
+                startActivity(intent1);
+            }
+        });
+
+        Button button2 = findViewById(R.id.buttonZurückZurMainActivity);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(risikopatient_erstellen.this, MainActivity.class);
+                Intent intent2 = new Intent(risikopatient_erstellen.this, MainActivity.class);
 
-                startActivity(intent);
+                startActivity(intent2);
             }
         });
 
--- app/src/main/java/com/example/sturzapp/risikopatient_startseite.java
+++ app/src/main/java/com/example/sturzapp/risikopatient_startseite.java
@@ -3,15 +3,22 @@ package com.example.sturzapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class risikopatient_startseite extends AppCompatActivity {
-
-    String email = getIntent().getStringExtra("email");
-editTextEmail.setText(email);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_risikopatient_startseite);
+
+        String email = getIntent().getStringExtra("e_mail");
+
+        TextView textView = findViewById(R.id.textViewEmail);
+
+        textView.setText(email);
+
+
+
     }
 }
