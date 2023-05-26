24937fcee060e72f0e7f2ec4bde646e65e0aeba8
a61y23 <philipp.weinhandl@edu.fh-joanneum.at>
2023-05-26 10:26:36 +0200

    startseite Risikopatient

---
+++ app/src/main/java/com/example/sturzapp/risikopatient_startseite.java
@@ -0,0 +1,17 @@
+package com.example.sturzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class risikopatient_startseite extends AppCompatActivity {
+
+    String email = getIntent().getStringExtra("email");
+editTextEmail.setText(email);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_risikopatient_startseite);
+    }
+}
