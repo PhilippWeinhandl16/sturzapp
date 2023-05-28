4c512f8406787f4ea96cd066f4c7aac056a54437
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-05-28 20:25:44 +0200

    Richtige Version

--- app/src/main/java/com/example/sturzapp/risikopatient_startseite.java
+++ app/src/main/java/com/example/sturzapp/risikopatient_startseite.java
@@ -3,22 +3,15 @@ package com.example.sturzapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class risikopatient_startseite extends AppCompatActivity {
+
+    String email = getIntent().getStringExtra("email");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_risikopatient_startseite);
-
-        String email = getIntent().getStringExtra("e_mail");
-
-        TextView textView = findViewById(R.id.textViewEmail);
-
-        textView.setText(email);
-
-
-
     }
 }
