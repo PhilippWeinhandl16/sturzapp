82f9ae4afc3f2b6fdac26e0c351d3baa06dbb40f
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-05-31 12:16:03 +0200

    neu

--- app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/Riskiopatient_Notfallkontakt_Hinzufuegen.java
+++ app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/Riskiopatient_Notfallkontakt_Hinzufuegen.java
@@ -3,6 +3,9 @@ package com.example.sturzapp.gui.risikopatient_gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sturzapp.R;
 
@@ -12,5 +15,27 @@ public class Riskiopatient_Notfallkontakt_Hinzufuegen extends AppCompatActivity 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_riskiopatient_notfallkontakt_hinzufuegen);
+
+        Button button_notfallkontakt_hinzufuegen = findViewById(R.id.Button_Notfallkontakt_Hinzufuegen);
+        button_notfallkontakt_hinzufuegen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText editText_eMail = findViewById(R.id.editTextEmail);
+                EditText editText_Vorname = findViewById(R.id.editTextVorname);
+                EditText editText_Nachname = findViewById(R.id.editTextNachname);
+                EditText editText_Strasse_und_Hausnummer = findViewById(R.id.EditText_Strasse_und_Hausnummer);
+                EditText editText_PLZ_und_Ort = findViewById(R.id.editPostleitzahl_und_Ort);
+
+                String e_mail = editText_eMail.getText().toString();
+                String vorname = editText_Vorname.getText().toString();
+                String nachname = editText_Nachname.getText().toString();
+                String strasse_und_hausnummer = editText_Strasse_und_Hausnummer.getText().toString();
+                String plz_und_ort = editText_PLZ_und_Ort.getText().toString();
+            }
+        });
+
+
+
     }
 }
