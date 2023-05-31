7254aed49c860696846ea2351f102b13308d0c71
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-05-31 11:57:02 +0200

    Neue Klasse (Notfallkontakt zu Risikopatient hinzufuengen) erstellt

---
+++ app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/Riskiopatient_Notfallkontakt_Hinzufuegen.java
@@ -0,0 +1,16 @@
+package com.example.sturzapp.gui.risikopatient_gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sturzapp.R;
+
+public class Riskiopatient_Notfallkontakt_Hinzufuegen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riskiopatient_notfallkontakt_hinzufuegen);
+    }
+}
