0c17ed4ae76adc5f715183930f30f7c13cdbdab4
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-06-14 12:16:49 +0200

    Verbesserungsvorschläge von Hr. Alcalde

--- app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/Risikopatient_Startseite.java
+++ app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/Risikopatient_Startseite.java
@@ -13,67 +13,45 @@ import com.example.sturzapp.R;
 
 public class Risikopatient_Startseite extends AppCompatActivity {
 
-        Intent intent = getIntent();
+    Intent intent = getIntent();
 
-        //Risikopatientdaten
-            private String rp_e_mail;
-            private String rp_passwort;
-            private String rp_vorname;
-            private String rp_nachname;
-            private String rp_adresse;
-            private String rp_plz;
+    //Risikopatientdaten
+    private String rp_e_mail;
+    private String rp_passwort;
+    private String rp_vorname;
+    private String rp_nachname;
+    private String rp_adresse;
+    private String rp_plz;
 
-            //Notfallkontaktdaten
-            private String nfk_name;
-            private String nfk_adresse;
-            private String nfk_plz;
-            private String nfk_email;
+    //Notfallkontaktdaten
+    private String nfk_name;
+    private String nfk_adresse;
+    private String nfk_plz;
+    private String nfk_email;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_risikopatient_startseite);
+
+        initializeEmail();
+
+        initializeControls();
+
+        //Button um Daten von Notfallkontakt zu ändern
+        Button Button2 = findViewById(R.id.buttonNotfallkontakt_aendern);
+        Button2.setOnClickListener(new View.OnClickListener() {
             @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_risikopatient_startseite);
+            public void onClick(View view) {
 
-                // Daten aus dem Intent abrufen
-                Intent intent = getIntent();
+                Intent intent2 = new Intent(Risikopatient_Startseite.this, Risikopatient_Notfallkontakt_Aendern.class);
+                initializeIntentWithData(intent2);
 
-                //E-Mail des Risikopatienten in View anzeigen lassen
-                String e_mail = intent.getStringExtra("rp_e_mail");
-                TextView textViewEmail = findViewById(R.id.textViewEmail);
-                textViewEmail.setText(e_mail);
+                startActivity(intent2);
+            }
+        });
 
-                //Risikopatientendaten abspeichern
-                rp_e_mail = intent.getStringExtra("rp_e_mail");
-                rp_passwort = intent.getStringExtra("rp_passwort");
-                rp_vorname = intent.getStringExtra("rp_vorname");
-                rp_nachname = intent.getStringExtra("rp_nachname");
-                rp_adresse = intent.getStringExtra("rp_adresse");
-                rp_plz = intent.getStringExtra("rp_plz");
-
-                //Notfallkontaktdaten abspeichern
-                nfk_name = intent.getStringExtra("nfk_name");
-                nfk_adresse = intent.getStringExtra("nfk_adresse");
-                nfk_plz = intent.getStringExtra("nfk_plz");
-                nfk_email = intent.getStringExtra("nfk_email");
-
-
-                //Button um Daten von Notfallkontakt zu ändern
-                Button Button2 = findViewById(R.id.buttonNotfallkontakt_aendern);
-                Button2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        Intent intent2 = new Intent(Risikopatient_Startseite.this, Risikopatient_Notfallkontakt_Aendern.class);
-                        intent2.putExtra("nfk_name", nfk_name);
-                        intent2.putExtra("nfk_adresse", nfk_adresse);
-                        intent2.putExtra("nfk_plz", nfk_plz);
-                        intent2.putExtra("nfk_email", nfk_email);
-
-                        startActivity(intent2);
-                    }
-                });
-
-                // Daten in einem neuen Intent speichern
+        // Daten in einem neuen Intent speichern
 
               /*  intent2.putExtra("rp_e_mail", rp_e_mail);
                 intent2.putExtra("rp_passwort", rp_passwort);
@@ -83,12 +61,48 @@ public class Risikopatient_Startseite extends AppCompatActivity {
                 intent2.putExtra("rp_plz", rp_plz); */
 
 
-            }
+    }
 
 
+    private void initializeControls(){
+        //Risikopatientendaten abspeichern
+        rp_e_mail = intent.getStringExtra("rp_e_mail");
+        rp_passwort = intent.getStringExtra("rp_passwort");
+        rp_vorname = intent.getStringExtra("rp_vorname");
+        rp_nachname = intent.getStringExtra("rp_nachname");
+        rp_adresse = intent.getStringExtra("rp_adresse");
+        rp_plz = intent.getStringExtra("rp_plz");
 
+        //Notfallkontaktdaten abspeichern
+        nfk_name = intent.getStringExtra("nfk_name");
+        nfk_adresse = intent.getStringExtra("nfk_adresse");
+        nfk_plz = intent.getStringExtra("nfk_plz");
+        nfk_email = intent.getStringExtra("nfk_email");
 
+    }
 
+    /**
+     * This method initialize an intent with data.
+     * @param i: intent to initialize. Null values are ignored.
+     */
+    private void initializeIntentWithData(Intent i){
+        if(i!=null){
+            i.putExtra("nfk_name", nfk_name);
+            i.putExtra("nfk_adresse", nfk_adresse);
+            i.putExtra("nfk_plz", nfk_plz);
+            i.putExtra("nfk_email", nfk_email);
+        }
+    }
+
+    private void initializeEmail(){
+        // Daten aus dem Intent abrufen
+        Intent intent = getIntent();
+
+        //E-Mail des Risikopatienten in View anzeigen lassen
+        String e_mail = intent.getStringExtra("rp_e_mail");
+        TextView textViewEmail = findViewById(R.id.textViewEmail);
+        textViewEmail.setText(e_mail);
+    }
 
 
 
