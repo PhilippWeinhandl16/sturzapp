834eca16223b17dc4121510e233a6379188073aa
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-06-22 13:00:54 +0200

    aktuelle änderungen

--- app/src/main/java/com/example/sturzapp/MainActivity.java
+++ app/src/main/java/com/example/sturzapp/MainActivity.java
@@ -43,64 +43,42 @@ public class MainActivity extends AppCompatActivity {
         // Alle Konten aus der Datenbank abrufen
         getAllAccounts();
 
-        RadioGroup radioGroup_account_auswahl1 = findViewById(R.id.radioGroup_account_auswahl1);
+
+
+
+
 
         Button button_login = findViewById(R.id.buttonLogin);
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editText_emailadresse = findViewById(R.id.editText_Email);
-                EditText editText_passwort = findViewById(R.id.editTextPassword);
+                EditText editTextemailRP = findViewById(R.id.editTextemailRP);
+                EditText editTextpasswordRP = findViewById(R.id.editTextpasswordRP);
 
-                String email_adresse = editText_emailadresse.getText().toString();
-                String passwort = editText_passwort.getText().toString();
-
-                int selected_RadioButton = radioGroup_account_auswahl1.getCheckedRadioButtonId();
-                RadioButton radioButton = findViewById(selected_RadioButton);
-                String ausgewaehlterRadioButtonText = radioButton.getText().toString();
+                String email_adresse = editTextemailRP.getText().toString();
+                String passwort = editTextpasswordRP.getText().toString();
 
                 Intent intent;
 
             }
         });
 
-        RadioButton risikopatient2 = findViewById(R.id.radioButtonRiskPatient2);
-        RadioButton notfallkontakt2 = findViewById(R.id.radioButton_Notfallkontakt2);
-
         Button button_accountErstellen = findViewById(R.id.button_createAccount);
 
         button_accountErstellen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                RadioGroup radioGroup_account_auswahl2 = findViewById(R.id.radioGroup_account_auswahl2);
-                int selected_RadioButton = radioGroup_account_auswahl2.getCheckedRadioButtonId();
-                RadioButton radioButton = findViewById(selected_RadioButton);
 
-                if (radioButton != null) {
-                    String ausgewaehlterRadioButtonText = radioButton.getText().toString();
 
-                    Intent intent;
-
-                    if (ausgewaehlterRadioButtonText.equals("Risikopatient")) {
-                        intent = new Intent(MainActivity.this, Risikopatient_Erstellen.class);
-                    } else if (ausgewaehlterRadioButtonText.equals("Notfallkontakt")) {
-                        intent = new Intent(MainActivity.this, Notfallkontakt_Erstellen.class);
-                    } else {
-                        intent = new Intent(MainActivity.this, DefaultActivity.class);
-                    }
+                Intent  intent = new Intent(MainActivity.this, Risikopatient_Erstellen.class);
 
                     startActivity(intent);
-                } else {
-                    // Wenn kein RadioButton ausgewählt ist
-                    Toast.makeText(MainActivity.this, "Bitte wählen Sie eine Option aus", Toast.LENGTH_SHORT).show();
-                }
-            }
+        }
         });
 
         //Sturzerkennungs-Service starten
         Intent serviceIntent = new Intent(this, SturzerkennungsService.class);
         startService(serviceIntent);
-
 
     }
 
--- app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/Risikopatient_Erstellen.java
+++ app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/Risikopatient_Erstellen.java
@@ -20,19 +20,17 @@ public class Risikopatient_Erstellen extends AppCompatActivity {
 
 
         //Daten für den Risikopatienten
-        EditText editTextRP_Email = findViewById(R.id.editText_RP_Email);
-        EditText editTextRP_Passwort = findViewById(R.id.editTextRP_Passwort);
-        EditText editTextRP_firstName = findViewById(R.id.editTextRP_firstName);
-        EditText editTextRP_lastName = findViewById(R.id.editTextRP_lastName);
-        EditText editTextRP_address = findViewById(R.id.editTextRP_address);
-        EditText editTextRP_PLZ = findViewById(R.id.editTextRP_PLZ);
+        EditText editTextemailRP_input = findViewById(R.id.editTextemailRP_input);
+        EditText editTextpasswordRP_input = findViewById(R.id.editTextpasswordRP_input);
+        EditText editTextfirstNameRP_input = findViewById(R.id.editTextfirstNameRP_input);
+        EditText editTextlastNameRP_input = findViewById(R.id.editTextlastNameRP_input);
+
 
         //Daten für den Notfallkontakt des Risikopatienten
         //1. Vor- und Nachname, 2. Straße und Hausnummer, 3. PLZ und Ort, 4. E-Mail Adresse
-        EditText editTextNFK_Name = findViewById(R.id.editTextNFK_Name);
-        EditText editTextNFK_address = findViewById(R.id.editTextNFK_address);
-        EditText editTextNFK_PLZ = findViewById(R.id.editTextNFK_PLZ);
-        EditText editTextNFK_Email = findViewById(R.id.editTextNFK_Email);
+        EditText editTextemailNFK_input = findViewById(R.id.editTextemailNFK_input);
+        EditText editTextnameNFK_input = findViewById(R.id.editTextnameNFK_input);
+
 
         Button button = findViewById(R.id.buttonZurückZurMainActivity);
         button.setOnClickListener(new View.OnClickListener() {
@@ -55,34 +53,27 @@ public class Risikopatient_Erstellen extends AppCompatActivity {
 
 
                     //Eingaben der Daten für Risikopatienten speichern
-                    String rp_e_mail = editTextRP_Email.getText().toString();
-                    String rp_passwort = editTextRP_Passwort.getText().toString();
-                    String rp_firstName = editTextRP_firstName.getText().toString();
-                    String rp_lastName = editTextRP_lastName.getText().toString();
-                    String rp_address = editTextRP_address.getText().toString();
-                    String rp_plz = editTextRP_PLZ.getText().toString();
+                    String emailRP = editTextemailRP_input.getText().toString();
+                    String passwordRP = editTextpasswordRP_input.getText().toString();
+                    String firstNameRP = editTextfirstNameRP_input.getText().toString();
+                    String lastNameRP = editTextlastNameRP_input.getText().toString();
+
 
                     //Eingaben der Daten für Notfallkontakt speichern
-                    String nfk_name = editTextNFK_Name.getText().toString();
-                    String nfk_address = editTextNFK_address.getText().toString();
-                    String nfk_plz = editTextNFK_PLZ.getText().toString();
-                    String nfk_email = editTextNFK_Email.getText().toString();
+                    String emailNFK = editTextemailNFK_input.getText().toString();
+                    String nameNFK = editTextnameNFK_input.getText().toString();
 
                     Intent intent2 = new Intent(Risikopatient_Erstellen.this, Risikopatient_Startseite.class);
 
                     //Risikopatient Daten in Intent speichern
-                    intent2.putExtra("rp_e_mail", rp_e_mail);
-                    intent2.putExtra("rp_passwort", rp_passwort);
-                    intent2.putExtra("rp_firstName", rp_firstName);
-                    intent2.putExtra("rp_lastName", rp_lastName);
-                    intent2.putExtra("rp_address", rp_address);
-                    intent2.putExtra("rp_plz", rp_plz);
+                    intent2.putExtra("emailRP", emailRP);
+                    intent2.putExtra("passwordRP", passwordRP);
+                    intent2.putExtra("firstNameRP", firstNameRP);
+                    intent2.putExtra("lastNameRP", lastNameRP);
 
                     //Notfallkontaktdaten zu Risikopatient in Intent speichern
-                    intent2.putExtra("nfk_name", nfk_name);
-                    intent2.putExtra("nfk_address", nfk_address);
-                    intent2.putExtra("nfk_plz", nfk_plz);
-                    intent2.putExtra("nfk_email", nfk_email);
+                    intent2.putExtra("emailNFK", emailNFK);
+                    intent2.putExtra("nameNFK", nameNFK);
 
                     startActivity(intent2);
 
