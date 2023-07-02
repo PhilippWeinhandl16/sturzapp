92deff60a2e476538f5841560cc40c0bc06b8e08
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-07-02 23:34:45 +0200

    RisikopatientNotfallbutton.java Javadoc hinzugefügt

--- app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/RisikopatientNotfallbutton.java
+++ app/src/main/java/com/example/sturzapp/gui/risikopatient_gui/RisikopatientNotfallbutton.java
@@ -14,6 +14,12 @@ import com.example.sturzapp.R;
 import com.example.sturzapp.database.SturzappDatabase;
 import com.example.sturzapp.database.entity.AccountEntity;
 
+/**
+ * Die Aktivität "RisikopatientNotfallbutton" ermöglicht es dem Risikopatienten, einen Notfall
+ * mit Klick auf einen Notfallbutton zu melden und es wird eine Email an den zuständigen Notfallkontakt
+ * ausgeschickt
+ * Die Aktivität enthält auch einen Button, um zur Startseite des Risikopatienten zurückzukehren.
+ */
 public class RisikopatientNotfallbutton extends AppCompatActivity {
 
     private Button button_notfallbutton;
@@ -28,6 +34,10 @@ public class RisikopatientNotfallbutton extends AppCompatActivity {
         setupButtonListeners();
     }
 
+    /**
+     * Initialisiert die Button der Aktivität
+     * Setzt die Hintergrundfarbe des Notfall-Buttons auf Rot
+     */
     public void initializeViews() {
         button_notfallbutton = findViewById(R.id.Button_Notfallbutton);
         ColorStateList colorStateList = ColorStateList.valueOf(getResources().getColor(R.color.red));
@@ -36,6 +46,10 @@ public class RisikopatientNotfallbutton extends AppCompatActivity {
         button_backToStartseite = findViewById(R.id.Button_backToStartseite);
     }
 
+    /**
+     * Legt die Button-Listener für den {@code button_notfallbutton} und den
+     * Button {@code button_backToStartseite} fest
+     */
     public void setupButtonListeners() {
         button_notfallbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,34 +66,40 @@ public class RisikopatientNotfallbutton extends AppCompatActivity {
         });
     }
 
+    /**
+     * Sendet eine Notfall-E-Mail an den zuständigen Notfallkontakt
+     * Holt die Account-Informationen aus der Datenbank und verwendet die E-Mail-Adresse des Kontakts
+     * Erstellt einen Intent zum Senden der E-Mail, startet die E-Mail-Activity und
+     * zeigt dem Benutzer eine Toast-Nachricht an, wenn keine E-Mail-App verfügbar ist
+     */
     public void sendEmergencyEmail() {
         SturzappDatabase db = SturzappDatabase.getInstance(getApplicationContext());
         new Thread(() -> {
-            // Account-Informationen aus der Datenbank abrufen
-            AccountEntity account = db.accountDao().getAccountById(1); // Ersetze 1 durch die entsprechende ID des eingeloggten Benutzers
+            AccountEntity account = db.accountDao().getAccountById(1);
 
             String subject = "Notfallmeldung";
             String body = "Ihr Risikopatient meldet einen Notfall! Womöglich benötigt er Hilfe";
 
-            // Intent erstellen, um die E-Mail zu senden
             Intent intent = new Intent(Intent.ACTION_SEND);
             intent.setType("message/rfc822");
             intent.putExtra(Intent.EXTRA_EMAIL, new String[]{account.getEmailNFK()});
             intent.putExtra(Intent.EXTRA_SUBJECT, subject);
             intent.putExtra(Intent.EXTRA_TEXT, body);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Setze die FLAG_ACTIVITY_NEW_TASK Flag
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
             PackageManager packageManager = getPackageManager();
             if (intent.resolveActivity(packageManager) != null) {
-                // E-Mail-Activity starten
                 startActivity(intent);
             } else {
-                // Keine E-Mail-App verfügbar
                 Toast.makeText(getApplicationContext(), "Keine E-Mail-App gefunden", Toast.LENGTH_SHORT).show();
             }
         }).start();
     }
 
+    /**
+     * Bei Klick auf den Button {@code button_backToStartseite} wird zurück zur
+     * {@link RisikopatientStartseite} navigiert
+     */
     public void navigateToRisikopatientStartseite() {
         onBackPressed();
     }
