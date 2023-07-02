c5d13d642414b1057fd695d7fc2b1085a8c35928
manserda21 <daniel.manser@edu.fh-joanneum.at>
2023-07-02 23:13:25 +0200

    AccountEntity.java + SturzappDatabase.java- Javadoc hinzugefügt + Kommentare entfernt

--- app/src/main/java/com/example/sturzapp/database/SturzappDatabase.java
+++ app/src/main/java/com/example/sturzapp/database/SturzappDatabase.java
@@ -10,14 +10,29 @@ import androidx.room.RoomDatabase;
 import com.example.sturzapp.database.dao.AccountDao;
 import com.example.sturzapp.database.entity.AccountEntity;
 
+/**
+ * Die Klasse {@code SturzappDatabase} repräsentiert die Room-Datenbank der Sturzmart-App
+ * Sie enthält die Definition der Datenbank und die Zugriffsmethoden auf die DAOs
+ */
 @Database(entities = {AccountEntity.class}, version = 1)
 public abstract class SturzappDatabase extends RoomDatabase {
     private static final String DATABASE_NAME = "sturzapp_db";
 
     private static SturzappDatabase instance;
 
+    /**
+     * Gibt das {@link AccountDao} zurück,
+     * das für den Zugriff auf die Account-Entitäten in der Datenbank verwendet wird
+     * @return das AccountDao-Objekt
+     */
     public abstract AccountDao accountDao();
 
+    /**
+     * Gibt die einzige Instanz der Sturzapp-Datenbank zurück.
+     * Wenn noch keine Instanz vorhanden ist, wird eine neue Instanz erstellt
+     * @param context der Kontext der Anwendung
+     * @return die Sturzapp-Datenbankinstanz
+     */
     public static synchronized SturzappDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
--- app/src/main/java/com/example/sturzapp/database/entity/AccountEntity.java
+++ app/src/main/java/com/example/sturzapp/database/entity/AccountEntity.java
@@ -4,6 +4,11 @@ import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
 
+/**
+ * Die Klasse {@code AccountEntity} repräsentiert ein Account-Objekt in der
+ * {@link com.example.sturzapp.database.SturzappDatabase}
+ * Sie wird als Entity in der Room-Datenbank verwendet und enthält die Attribute eines Accounts
+ */
 @Entity(tableName = "accounts")
 public class AccountEntity {
 
@@ -16,9 +21,21 @@ public class AccountEntity {
     private String emailNFK;
     private String nameNFK;
 
+    /**
+     * Konstruktor für einen leeren Account.
+     */
     public AccountEntity() {
     }
 
+    /**
+     * Konstruktor für einen Account mit allen Attributen.
+     * @param emailRP     die E-Mail-Adresse des Accounts (RP steht für Risikopatient)
+     * @param passwordRP  das Passwort des Accounts (RP steht für Risikopatient)
+     * @param firstNameRP der Vorname des Accounts (RP steht für Risikopatient)
+     * @param lastNameRP  der Nachname des Accounts (RP steht für Risikopatient)
+     * @param emailNFK    die E-Mail-Adresse des Accounts (NFK steht für Notfallkontakt)
+     * @param nameNFK     der Name des Accounts (NFK steht für Notfallkontakt)
+     */
     public AccountEntity(String emailRP, String passwordRP, String firstNameRP, String lastNameRP, String emailNFK, String nameNFK) {
         this.emailRP = emailRP;
         this.passwordRP = passwordRP;
@@ -28,66 +45,124 @@ public class AccountEntity {
         this.nameNFK = nameNFK;
     }
 
-    // Getter-Methoden
+    /**
+     * Gibt die ID des Accounts zurück
+     * @return die ID des Accounts
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gibt die E-Mail-Adresse des Risikopatienten zurück
+     * @return die E-Mail-Adresse des Accounts
+     */
     public String getEmailRP() {
         return emailRP;
     }
 
+    /**
+     * Gibt das Passwort des Risikopatienten zurück
+     * @return das Passwort des Accounts
+     */
     public String getPasswordRP() {
         return passwordRP;
     }
 
+    /**
+     * Gibt den Vornamen des Risikopatienten zurück
+     * @return der Vorname des Accounts
+     */
     public String getFirstNameRP() {
         return firstNameRP;
     }
 
+    /**
+     * Gibt den Nachnamen des Risikopatienten zurück
+     * @return der Nachname des Accounts
+     */
     public String getLastNameRP() {
         return lastNameRP;
     }
 
+    /**
+     * Gibt die E-Mail-Adresse des Notfallkontakts zurück
+     * @return die E-Mail-Adresse des Notfallkontakts
+     */
     public String getEmailNFK() {
         return emailNFK;
     }
 
+    /**
+     * Gibt den Namen des Notfallkontakts zurück
+     * @return der Name des Notfallkontakts
+     */
     public String getNameNFK() {
         return nameNFK;
     }
 
-
-    //Setter-Methoden
+    /**
+     * Setzt die ID des Accounts
+     * @param id die ID des Accounts
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Setzt die E-Mail-Adresse des Accounts
+     * @param emailRP die E-Mail-Adresse des Accounts
+     */
     public void setEmailRP(String emailRP) {
         this.emailRP = emailRP;
     }
 
+    /**
+     * Setzt das Passwort des Accounts
+     * @param passwordRP das Passwort des Accounts
+     */
     public void setPasswordRP(String passwordRP) {
         this.passwordRP = passwordRP;
     }
 
+    /**
+     * Setzt den Vornamen des Accounts
+     * @param firstNameRP der Vorname des Accounts
+     */
     public void setFirstNameRP(String firstNameRP) {
         this.firstNameRP = firstNameRP;
     }
 
+    /**
+     * Setzt den Nachnamen des Accounts
+     * @param lastNameRP der Nachname des Accounts
+     */
     public void setLastNameRP(String lastNameRP) {
         this.lastNameRP = lastNameRP;
     }
 
+    /**
+     * Setzt die E-Mail-Adresse des Notfallkontakts
+     * @param emailNFK die E-Mail-Adresse des Notfallkontakts
+     */
     public void setEmailNFK(String emailNFK) {
         this.emailNFK = emailNFK;
     }
 
+    /**
+     * Setzt den Namen des Notfallkontakts
+     * @param nameNFK der Name des Notfallkontakts
+     */
     public void setNameNFK(String nameNFK) {
         this.nameNFK = nameNFK;
     }
 
-
+    /**
+     * Gibt eine textuelle Repräsentation des Account-Objekts zurück
+     * Die Ausgabe besteht aus dem Vor- und Nachnamen des Accounts
+     * gefolgt von einem Bindestrich und der E-Mail-Adresse des Accounts.
+     * @return eine textuelle Repräsentation eines Account-Objekts
+     */
     @Override
     public String toString() {
         return firstNameRP + lastNameRP + " - " + emailRP;
